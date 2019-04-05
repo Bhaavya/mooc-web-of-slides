63eb5af06d5cef5b64b10078fc544440369e72ef
Sahiti Labhishetty <sahiti@wirelessprv-10-192-34-5.near.illinois.edu>
2019-04-04 19:23:02 -0500

    pruned results more and removed repeated links

--- UI/model.py
+++ UI/model.py
@@ -22,64 +22,64 @@ ss_corpus = pickle.load(open('static/tf_idf_outputs/ss_corpus.p', 'rb'))
 
 
 def get_snippet_sentences(slide_name, matching_keywords):
-	idx = slide_names.index(slide_name)
-	content = ss_corpus[idx].split(' ')
-	include  = [0]*len(content)
-	for c in range(len(content)):
-		if content[c] in matching_keywords:
-			for i in range(max(0,c-2), min(c+3,len(content))):
-				include[i] = 1
-	text = '' 
-	for c in range(len(content)):
-		if include[c]== 1:
-			if c!=0 and include[c-1] == 0:
-				text += '......'
-			text += content[c] + ' '
-	text += '......'
-	return text
+    idx = slide_names.index(slide_name)
+    content = ss_corpus[idx].split(' ')
+    include  = [0]*len(content)
+    for c in range(len(content)):
+        if content[c] in matching_keywords:
+            for i in range(max(0,c-2), min(c+3,len(content))):
+                include[i] = 1
+    text = '' 
+    for c in range(len(content)):
+        if include[c]== 1:
+            if c!=0 and include[c-1] == 0:
+                text += '......'
+            text += content[c] + ' '
+    text += '......'
+    return text
 
 def trim_name(slide_name):
-	name = slide_name.split(' ')
-	new_name = []
-	for n in name:
-		if (len(n) > 2) and (len(re.findall('[0-9\.]+', n)) == 0):
-			if (n == 'Lesson') or (n=='Part'):
-				continue
-			new_name += [n]
+    name = slide_name.split(' ')
+    new_name = []
+    for n in name:
+        if (len(n) > 2) and (len(re.findall('[0-9\.]+', n)) == 0):
+            if (n == 'Lesson') or (n=='Part'):
+                continue
+            new_name += [n]
 
-	return ' '.join(new_name)
+    return ' '.join(new_name)
 
 def get_color(slide_course_name, related_slide_course_name):
-	if slide_course_name==related_slide_course_name:
-		return "blue"
-	else:
-		return "brown"
+    if slide_course_name==related_slide_course_name:
+        return "blue"
+    else:
+        return "brown"
 
 def get_snippet(slide_name, related_slide_name):
-	no_keywords = False
-	related_slide_name = related_slide_name.replace('----', '##')[:-4]
-	slide_name = slide_name.replace('----', '##')[:-4]
-	idx1 = slide_names.index(slide_name)
-	idx2 = slide_names.index(related_slide_name)	
-	title_tfidf1 = title_tfidfs[idx1,:]
-	title_tfidf2 = title_tfidfs[idx2,:]
-	tfidf1 = tfidfs[idx1,:]
-	tfidf2 = tfidfs[idx2,:]
-	term_sims = 2.8956628*(title_tfidf1*title_tfidf2)+ 5.92724651*(tfidf1*tfidf2)
-	top_terms_indeces = np.argsort(term_sims)[::-1][:5]
-	#print related_slide_name
-	#print np.sort(term_sims)[::-1][:10]
-	top_terms_indeces = filter(lambda l : term_sims[l]>0, top_terms_indeces)
-	matching_words = [vocabulary_list[t] for t in top_terms_indeces]
-	#matching_words = [(vocabulary_list[t],vec.idf_[t]) for t in top_terms_indeces]
-	#matching_words = sorted(matching_words, key = lambda l :l[1], reverse = True)
-	#matching_words = map(lambda l : l[0], matching_words)
-	if len(matching_words) == 0 :
-		no_keywords = True
-	keywords = ', '.join(matching_words) 
-	snippet_sentence =  get_snippet_sentences(related_slide_name, matching_words)
+    no_keywords = False
+    related_slide_name = related_slide_name.replace('----', '##')[:-4]
+    slide_name = slide_name.replace('----', '##')[:-4]
+    idx1 = slide_names.index(slide_name)
+    idx2 = slide_names.index(related_slide_name)    
+    title_tfidf1 = title_tfidfs[idx1,:]
+    title_tfidf2 = title_tfidfs[idx2,:]
+    tfidf1 = tfidfs[idx1,:]
+    tfidf2 = tfidfs[idx2,:]
+    term_sims = 2.8956628*(title_tfidf1*title_tfidf2)+ 5.92724651*(tfidf1*tfidf2)
+    top_terms_indeces = np.argsort(term_sims)[::-1][:5]
+    #print related_slide_name
+    #print np.sort(term_sims)[::-1][:10]
+    top_terms_indeces = filter(lambda l : term_sims[l]>0, top_terms_indeces)
+    matching_words = [vocabulary_list[t] for t in top_terms_indeces]
+    #matching_words = [(vocabulary_list[t],vec.idf_[t]) for t in top_terms_indeces]
+    #matching_words = sorted(matching_words, key = lambda l :l[1], reverse = True)
+    #matching_words = map(lambda l : l[0], matching_words)
+    if len(matching_words) == 0 :
+        no_keywords = True
+    keywords = ', '.join(matching_words) 
+    snippet_sentence =  get_snippet_sentences(related_slide_name, matching_words)
 
-	return (('Slide title : ' + title_mapping[related_slide_name][:-1] +'\n' + 'Matching keywords: ' + keywords + '\n' + 'Snippet:' + snippet_sentence),no_keywords)
+    return (('Slide title : ' + title_mapping[related_slide_name][:-1] +'\n' + 'Matching keywords: ' + keywords + '\n' + 'Snippet:' + snippet_sentence),no_keywords)
 
 idx = metapy.index.make_inverted_index('slides-config.toml')
 ranker = metapy.index.OkapiBM25()
@@ -104,6 +104,10 @@ def load_related_slides():
         related_dict[key] = []
         for col_num in range(1,len(cols),2):
             pdf_name = cols[col_num].replace('##','----')+'.pdf'
+            if cols[col_num+1].strip() != '':
+		        score = float(cols[col_num+1].strip())
+		        if score < 0.03:
+		        	break
             name_comp = pdf_name.split('----')
             course_name = name_comp[0]
             lec_name ='----'.join(name_comp[1:-1])
@@ -113,7 +117,7 @@ def load_related_slides():
 def sort_slide_names(l): 
     """ Sort the given iterable in the way that humans expect.""" 
     convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
 
 def get_slide(course_name,slide,lno):
@@ -158,37 +162,45 @@ def get_prev_slide(course_name,lno,curr_slide):
     return prev_slide ,lno,lectures[lno],related_slides_info,lectures,range(len(lectures))    
 
 def get_related_slides(slide_name):
-	if related_dict=={}:
-		load_related_slides()
-	related_slides = related_dict[slide_name]
-	disp_strs = []
-	disp_colors = []
-	disp_snippets= []
-	course_names = []
-	lnos = []
-	slide_comp = slide_name.split('----')
-	for r in related_slides:
-		comp = r.split('----')
-		#disp_strs.append(' '.join(comp[0].replace('_','-').split('-')).title() + ' : ' + ' '.join(comp[-2].replace('.txt','').replace('_','-').split('-')).title() + ' , ' + ' '.join(comp[-1].replace('.pdf','').split('-')).title())
-		related_slide_name = ' '.join(comp[-2].replace('.txt','').replace('_','-').split('-')).title() 
-		slide_course_name = ' '.join(slide_comp[0].replace('_','-').split('-')).title()
-		related_slide_course_name = ' '.join(comp[0].replace('_','-').split('-')).title()
-		color = get_color(slide_course_name, related_slide_course_name)
-		snippet,no_keywords = get_snippet(slide_name, r)
-		if no_keywords==True:
-			continue
-		disp_strs.append(' '.join(comp[0].replace('_','-').split('-')).title() + ' : ' + trim_name(related_slide_name))
-		disp_snippets.append(snippet)
-		disp_colors.append(color)
-		course_names.append(comp[0])
-		lectures = sort_slide_names(os.listdir(os.path.join(slides_path, comp[0])))
-		lnos.append(lectures.index('----'.join(comp[1:-1])))
-	return len(disp_strs),related_slides,disp_strs,course_names,lnos,disp_colors,disp_snippets
+    if related_dict=={}:
+        load_related_slides()
+    related_slides = related_dict[slide_name]
+    disp_strs = []
+    disp_colors = []
+    disp_snippets= []
+    course_names = []
+    lnos = []
+    slide_comp = slide_name.split('----')
+    related_slide_trim_names = []
+    for r in related_slides:
+        comp = r.split('----')
+        #disp_strs.append(' '.join(comp[0].replace('_','-').split('-')).title() + ' : ' + ' '.join(comp[-2].replace('.txt','').replace('_','-').split('-')).title() + ' , ' + ' '.join(comp[-1].replace('.pdf','').split('-')).title())
+        related_slide_name = ' '.join(comp[-2].replace('.txt','').replace('_','-').split('-')).title() 
+        slide_course_name = ' '.join(slide_comp[0].replace('_','-').split('-')).title()
+        related_slide_course_name = ' '.join(comp[0].replace('_','-').split('-')).title()
+        trimmed_name = ' '.join(comp[0].replace('_','-').split('-')).title() + ' : ' + trim_name(related_slide_name)
+        if trimmed_name in related_slide_trim_names:
+        	continue
+        else:
+        	related_slide_trim_names += [trimmed_name]
+        color = get_color(slide_course_name, related_slide_course_name)
+        snippet,no_keywords = get_snippet(slide_name, r)
+        if no_keywords==True:
+            continue
+        disp_strs.append(' '.join(comp[0].replace('_','-').split('-')).title() + ' : ' + trim_name(related_slide_name))
+        disp_snippets.append(snippet)
+        disp_colors.append(color)
+        course_names.append(comp[0])
+        lectures = sort_slide_names(os.listdir(os.path.join(slides_path, comp[0])))
+        lnos.append(lectures.index('----'.join(comp[1:-1])))
+    return len(disp_strs),related_slides,disp_strs,course_names,lnos,disp_colors,disp_snippets
 
 def get_search_results(search):
     query = metapy.index.Document()
     query.content(search)
+    print 'came here'
     top_docs = ranker.score(idx, query, num_results=10)
+    print 'got results'
     results = [slide_titles[x[0]] for x in top_docs]
     disp_strs = []
     course_names = []
@@ -200,7 +212,7 @@ def get_search_results(search):
         lectures = sort_slide_names(os.listdir(os.path.join(slides_path, comp[0])))
         lnos.append(lectures.index('----'.join(comp[1:-1])))
     for x in range(len(results)):
-    	results[x] = results[x].replace('##', '----') + '.pdf'
+        results[x] = results[x].replace('##', '----') + '.pdf'
     return len(results),results,disp_strs,course_names,lnos
 
 
